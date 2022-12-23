fd64f1dee0169ebcbed0137b0c4fe46bf5ca3818
Rafael Suazo <supahotfire16@hotmail.com>
2022-12-22 20:41:25 -0600

    0.1

---
+++ src/app/components/poke-table/poke-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  templateUrl: './poke-table.component.html',
+  styleUrls: ['./poke-table.component.scss']
+})
+export class PokeTableComponent {
+
+}
